f9541d3797b55646d1965040e77162b638cfc5d5
wangguoyan <717338097@qq.com>
2024-01-02 13:32:00 +0800

    update

--- code/java/uploadFile/src/main/java/com/example/upload/UpgradeSEE.java
+++ code/java/uploadFile/src/main/java/com/example/upload/UpgradeSEE.java
@@ -6,25 +6,47 @@ import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
 
-public class SSHFileUploader {
+public class UpgradeSEE {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
 
         List<String> strings = Arrays.asList(
-                "10.20.144.166,root,Test@orca,/Users/wgy/Downloads/upgrade,/home/wgy/upgrade,mysql"
-                , "10.20.144.165,wgy,Test@orca,/Users/wgy/Downloads/upgrade,/home/wgy/upgrade,lightdb"
-                , "10.40.2.24,root,deploy@r#dcenter9,C:\\Users\\wanggy29750\\Downloads\\sync,/home/wgy/upgrade,mysql"
-//              ,  "10.20.158.249,root,r#dcenter8,/Users/wgy/Downloads/sync,/home/update"
-
+//                "10.20.45.174,root,Preview@see2023,/Users/wgy/Downloads/upgrade,/home/wgy/upgrade,mysql",
+                "10.20.144.166,root,Test@orca,/Users/wgy/Downloads/upgrade,/home/wgy/upgrade,mysql",
+//                "10.20.144.165,wgy,Test@orca,/Users/wgy/Downloads/upgrade,/home/wgy/upgrade,lightdb",
+//               "10.20.158.249,root,r#dcenter8,/Users/wgy/Downloads/sync,/home/update",
+                ""
         );
+        // 是否删除文件·
+        AtomicBoolean deleteFlag = new AtomicBoolean(false); // 默认值
+        if (args != null && args.length > 0) {
+            // java -jar 调用
+            strings = Arrays.asList(args);
+            strings = strings.stream().map(s -> {
+                        if (s.startsWith("--delete=")) {
+                            deleteFlag.set(Boolean.parseBoolean(s.substring(s.indexOf("=") + 1)));
+                            System.out.println("是否删除文件: " + deleteFlag);
+                            return null;
+                        }
+                        if (s.startsWith("--config=")) {
+                            System.out.println("config :" + s);
+                            return s.substring(s.indexOf("=") + 1);
+                        }
+                        return null;
+                    }
+            ).filter(Objects::nonNull).collect(Collectors.toList());
+        }
 
         String envCommand = " source ~/.bash_profile  ";
-
-
         strings.forEach(s -> {
             String[] split = s.split(",");
+            if (split.length != 6) {
+                return;
+            }
             String host = split[0];
             String username = split[1];
             String password = split[2];
@@ -49,24 +71,32 @@ public class SSHFileUploader {
                                 if (type.equals("mysql") && file.getName().contains("lightdb")) {
                                     continue;
                                 }
-                                System.out.println("开始升级服务器：" + host + " 文件：" + file.getName());
+                                System.out.println("开始升级服务器：" + host + " 文件：" + file.getName() + "开始。。。");
                                 upload(session, file, remoteFolderPath + "/" + file.getName());
                                 // 删除文件
-                                file.delete();
+                                if (deleteFlag.get()) {
+                                    file.delete();
+                                }
                                 // 执行远程命令
                                 execCommand(session, " cd " + remoteFolderPath + " && rm -fr see ", 60);
                                 execCommand(session, " cd " + remoteFolderPath + " && unzip " + file.getName(), 60);
-                                File upgradeShellFile = write(type);
-                                upload(session, upgradeShellFile, remoteFolderPath + "/see/" + upgradeShellFile.getName());
-                                upgradeShellFile.delete();
-                                execCommand(session, envCommand + "&& cd " + remoteFolderPath + "/see && chmod 750 upgradeseeOnServer.sh && sh upgradeseeOnServer.sh  ", 180);
+                                if (type.equals("lightdb")) {
+                                    execCommand(session, envCommand + "&& cd " + remoteFolderPath + "/see && sh lightdb_upgrade.sh", 120);
+                                } else {
+                                    execCommand(session, envCommand + "&& cd " + remoteFolderPath + "/see && sh upgrade.sh", 120);
+
+                                }
                                 execCommand(session, " cd " + remoteFolderPath + " && rm -fr " + file.getName(), 60);
                                 execCommand(session, " cd " + remoteFolderPath + " && rm -fr see ", 60);
+
                                 if (type.equals("lightdb")) {
-                                    execCommand(session, envCommand + "&& cd $SEE20_INSTALL_HOME && rm -fr tomcat/.lightdb_sql_md5.properties  &&  sh lightdb_startUp.sh   ", 360);
+//                                    execCommand(session, envCommand + "&& cd $SEE20_INSTALL_HOME && rm -fr tomcat/.lightdb_sql_md5.properties  &&  sh lightdb_startUp.sh   ", 360);
+                                    execCommand(session, envCommand + "&& cd $SEE20_INSTALL_HOME   &&  sh lightdb_startUp.sh   ", 360);
                                 } else {
-                                    execCommand(session, envCommand + "&& cd $SEE20_INSTALL_HOME && rm -fr tomcat/.sql_md5.properties  &&  sh startUp.sh  ", 360);
+                                    //  execCommand(session, envCommand + "&& cd $SEE20_INSTALL_HOME && rm -fr tomcat/.sql_md5.properties  &&  sh startUp.sh  ", 360);
+                                    execCommand(session, envCommand + "&& cd $SEE20_INSTALL_HOME   &&  sh startUp.sh  ", 360);
                                 }
+                                System.out.println("升级服务器：" + host + " 文件：" + file.getName() + "结束。。。");
                             }
                         }
                     }
@@ -79,38 +109,6 @@ public class SSHFileUploader {
         System.exit(1);
     }
 
-    private static File write(String type) throws Exception {
-        String cmd = "set -e\n" +
-                "set -x\n" +
-                "\n" +
-                "if command -v expect &>/dev/null; then\n" +
-                "    echo \"expect 命令存在\"\n" +
-                "else\n" +
-                "    echo \"expect 命令不存在\"\n" +
-                "    exit\n" +
-                "fi\n" +
-                "\n" +
-                "expect -c \"\n" +
-                "  set timeout -1\n";
-        if (type.equals("lightdb")) {
-            cmd = cmd + "  spawn sh lightdb_upgrade.sh\n";
-        } else {
-            cmd = cmd + "  spawn sh upgrade.sh\n";
-        }
-        cmd = cmd + "  expect {\n" +
-                "     \\\"*Back up see*\\\"      {send \\\"no\\r \\\";exp_continue}\n" +
-                "     \\\"*successfully upgraded*\\\"      {send \\\"no\\r \\\";exp_continue}\n" +
-                "     expect eof\n" +
-                "  }\n" +
-                "  \"\n";
-        String filePath = "upgradeseeOnServer.sh";
-        // 创建一个输出流
-        FileOutputStream outputStream = new FileOutputStream(filePath);
-        outputStream.write(cmd.getBytes(StandardCharsets.UTF_8));
-        // 关闭流
-        outputStream.close();
-        return new File(filePath);
-    }
 
     private static void execCommand(Session session, String command, Integer timeout) throws Exception {
         System.out.println("远程服务器执行命令：" + command + " 开始...");
@@ -119,6 +117,8 @@ public class SSHFileUploader {
         channelExec.setCommand(command);
         InputStream commandOutput = channelExec.getInputStream();
         channelExec.connect();
+        // 获取输入和输出流
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(channelExec.getOutputStream()));
         Future<?> future = executorService.submit(() -> {
             try {
                 // 读取并打印命令输出
@@ -126,6 +126,14 @@ public class SSHFileUploader {
                 String line;
                 while ((line = reader.readLine()) != null) {
                     System.out.println(line);
+                    if (line.contains("[note] check last install workspace end ......") || line.contains("The SEE2.0 main program upgrade is complete.")) {
+                        writer.write("no\n");
+                        writer.flush();
+                    }
+                    if (line.contains("is successfully started.")) {
+                        System.out.println("升级SEE2.0成功...");
+                        break;
+                    }
                 }
             } catch (Exception ignored) {
             }
@@ -152,6 +160,7 @@ public class SSHFileUploader {
         // 上传文件
         final double[] uploadedBytes = {0};
         double totalSize = file.length();
+        final double[] speed = {0};
         channelSftp.put(file.getAbsolutePath(), remoteFilePath, new SftpProgressMonitor() {
             private final Timer timer = new Timer();
 
@@ -164,13 +173,14 @@ public class SSHFileUploader {
                     @Override
                     public void run() {
                         double progress = (uploadedBytes[0] / totalSize) * 100;
-                        System.out.printf("Upload progress: %.2f%%\n", progress);
+                        System.out.printf("Upload progress: %.2f%% \n", progress);
                     }
                 }, interval, interval);
             }
 
             @Override
             public boolean count(long count) {
+                speed[0] = count;
                 uploadedBytes[0] += count;
                 return true;
             }
