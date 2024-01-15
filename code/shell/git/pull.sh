5367c2e9d480de49adcf1ee591470d2fe7f74f51
wangguoyan <717338097@qq.com>
2024-01-15 10:37:13 +0800

    update

--- code/shell/git/pull.sh
+++ code/shell/git/pull.sh
@@ -1,7 +1,8 @@
 
-
 #sh /Users/wgy/Downloads/work/wangguoyan/code/shell/git/pull.sh
 #./pull.sh
+
+git stash
 
 git config --global http.proxy 127.0.0.1:15236
 git config --global https.proxy 127.0.0.1:15236
@@ -10,6 +11,7 @@ git config --global pull.rebase true
 
 git pull
 
-
 git config --global --unset http.proxy
 git config --global --unset https.proxy
+
+git stash pop
--- code/shell/git/push.sh
+++ code/shell/git/push.sh
@@ -1,13 +1,14 @@
 
 # sh /Users/wgy/Downloads/work/wangguoyan/code/shell/git/push.sh
 
+git stash
 
 git config --global http.proxy 127.0.0.1:15236
 git config --global https.proxy 127.0.0.1:15236
 
-
 git push
-
 
 git config --global --unset http.proxy
 git config --global --unset https.proxy
+
+git stash pop
